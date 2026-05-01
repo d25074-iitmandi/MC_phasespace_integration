8aa3e00b7fa9b369cc9dc090316851be17cace42
Abhas Rathi <255797915+d25074-iitmandi@users.noreply.github.com>
2026-05-02 02:50:06 +0530

    writing scaling file

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,6 @@
 #include "utils.h"
 #include <fstream>
-
+//Writing the output for scaling
 void save_results(int threads, double time, double speedup) {
     std::ofstream file("results/scaling.txt", std::ios::app);
     file << threads << " " << time << " " << speedup << "\n";
