8839cfe6117c629574f82ad539cb7ea7a5ea970c
Abhas Rathi <255797915+d25074-iitmandi@users.noreply.github.com>
2026-05-02 01:50:02 +0530

    Modified the cross-section to account for spin

--- physics.cpp
+++ physics.cpp
@@ -1,16 +1,13 @@
-#include<"physics.h">
-#include<cmath>
+#include "physics.h"
+#include <cmath>
 
-const double alpha = 1/137;
-const double vac_perm = 8.85*e-12;
-const double h = 6.626*e-34;
-const int c = 299792458;
-const double e2 = 2.0 * alpha * vac_perm * h * c;
+const double alpha = 1.0/137.0;
+const double e2 = 4.0 * M_PI * alpha;
 
 double matrix_element(double costheta){
-	return e2*e2*(1+costheta*costheta);
+	return e2*e2*0.5*(1.0+costheta*costheta);
 }
 
 double analytic_cross_section(double s){
-	return (4*M_PI*alpha*alpha)/(3*s);
+	return (4.0*M_PI*alpha*alpha)/(3.0*s);
 }
