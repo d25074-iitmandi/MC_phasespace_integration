29c0b46b4f6550b8b9f507970d74ae70a45637f4
Abhas Rathi <255797915+d25074-iitmandi@users.noreply.github.com>
2026-05-02 02:46:31 +0530

    compute matrix element and cross-section

--- src/physics.cpp
+++ src/physics.cpp
@@ -7,7 +7,7 @@ const double e2 = 4.0 * M_PI * alpha;
 double matrix_element(double costheta){
 	return e2*e2*0.5*(1.0+costheta*costheta);
 }
-
+ 
 double analytic_cross_section(double s){
 	return (4.0*M_PI*alpha*alpha)/(3.0*s);
 }
