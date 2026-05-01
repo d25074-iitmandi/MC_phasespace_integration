127ebb82e556e89dd718173f30cf958973aa647b
Abhas Rathi <255797915+d25074-iitmandi@users.noreply.github.com>
2026-05-02 01:36:27 +0530

    calculating matrix element and cross-section

--- physics.cpp
+++ physics.cpp
@@ -1,3 +1,4 @@
+#include<"physics.h">
 #include<cmath>
 
 const double alpha = 1/137;
@@ -6,3 +7,10 @@ const double h = 6.626*e-34;
 const int c = 299792458;
 const double e2 = 2.0 * alpha * vac_perm * h * c;
 
+double matrix_element(double costheta){
+	return e2*e2*(1+costheta*costheta);
+}
+
+double analytic_cross_section(double s){
+	return (4*M_PI*alpha*alpha)/(3*s);
+}
