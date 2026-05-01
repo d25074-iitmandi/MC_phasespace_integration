42d9bca6208f4fcd70f6c71d0559a214160c297b
Abhas Rathi <255797915+d25074-iitmandi@users.noreply.github.com>
2026-05-02 02:27:42 +0530

    phase space integration

---
+++ src/physics.cpp
@@ -0,0 +1,13 @@
+#include "physics.h"
+#include <cmath>
+
+const double alpha = 1.0/137.0;
+const double e2 = 4.0 * M_PI * alpha;
+
+double matrix_element(double costheta){
+	return e2*e2*0.5*(1.0+costheta*costheta);
+}
+
+double analytic_cross_section(double s){
+	return (4.0*M_PI*alpha*alpha)/(3.0*s);
+}
