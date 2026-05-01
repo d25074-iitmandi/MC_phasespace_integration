0346ed2847de610b31a5e55510deae17cd97bd5b
Abhas Rathi <255797915+d25074-iitmandi@users.noreply.github.com>
2026-05-02 01:09:31 +0530

    defining physics constants

---
+++ physics.cpp
@@ -0,0 +1,8 @@
+#include<cmath>
+
+const double alpha = 1/137;
+const double vac_perm = 8.85*e-12;
+const double h = 6.626*e-34;
+const int c = 299792458;
+const double e2 = 2.0 * alpha * vac_perm * h * c;
+
