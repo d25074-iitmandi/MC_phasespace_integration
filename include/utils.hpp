2619d750671e1db19652add1eb0aaf5a6a485533
Abhas Rathi <d25074@students.iitmandi.ac.in>
2026-05-03 00:31:26 +0530

    writing data for convergence and asymmetry

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,4 +2,5 @@
 #include <vector>
 
 void save_results(int threads, double time, double speedup);
-void save_convergence_results(int logN, double mc_sigma, double a_sigma, double abs_err, double mc_error);
+void save_convergence_results(int logN, int N, double mc_sigma, double a_sigma, double mc_error);
+void save_asymmetry(double sqrt_s, double a_fb, double a_fb_err);
