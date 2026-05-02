d900796aa18bedccadb54aea43360f0d776465ea
Abhas Rathi <d25074@students.iitmandi.ac.in>
2026-05-02 16:19:17 +0530

    angular term for phase space integration

--- src/integrator.cpp
+++ src/integrator.cpp
@@ -1,18 +1,18 @@
-#include "integrator.h"
-#include "physics.h"
-#include "rng.h"
+#include "integrator.hpp"
+#include "physics.hpp"
+#include "rng.hpp"
 #include <cmath>
 #include <omp.h>
 
-Result monte_carlo_serial(int N, double s) {
+Result monte_carlo_serial(int N, double s_sqrt) {
     RNG rng(42);
 
     double sum_w = 0.0, sum_w2 = 0.0; //Weights
 
     for (int i = 0; i < N; i++) {
         double costh = 2.0 * rng.uniform() - 1.0;
-        double M2 = matrix_element(costh);
-        double w = M2 / (32.0 * M_PI * M_PI * s);
+        double M2 = matrix_element(costh, s_sqrt);
+        double w = M2 / (32.0 * M_PI * M_PI * s_sqrt*s_sqrt);
 
         sum_w += w;
         sum_w2 += w * w;
@@ -25,7 +25,7 @@ Result monte_carlo_serial(int N, double s) {
     return {sigma, volume * sqrt(var)};
 }
 
-Result monte_carlo_openmp(int N, double s, int nthreads) {
+Result monte_carlo_openmp(int N, double s_sqrt, int nthreads) {
     double sum_w = 0.0, sum_w2 = 0.0;
 
     omp_set_num_threads(nthreads);
@@ -37,9 +37,11 @@ Result monte_carlo_openmp(int N, double s, int nthreads) {
         #pragma omp for reduction(+:sum_w,sum_w2)
         for (int i = 0; i < N; i++) {
             double costh = 2.0 * rng.uniform() - 1.0;
-            double M2 = matrix_element(costh);
-            double w = M2 / (32.0 * M_PI * M_PI * s);
-
+            double M2 = matrix_element(costh, s_sqrt);
+            double mu_mass = 0.1056;
+            double beta = sqrt(1.0 - 4.0 * pow(mu_mass, 2) / (s_sqrt * s_sqrt));
+            double w = beta * M2 / (32.0 * M_PI * M_PI * s_sqrt * s_sqrt);
+            
             sum_w += w;
             sum_w2 += w * w;
         }
