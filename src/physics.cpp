e186a719aba87255ea8601ea208f2557733df329
Abhas Rathi <d25074@students.iitmandi.ac.in>
2026-05-02 16:18:55 +0530

    additional term in matrix element and cross-section

--- src/physics.cpp
+++ src/physics.cpp
@@ -1,13 +1,19 @@
-#include "physics.h"
+#include "physics.hpp"
 #include <cmath>
 
 const double alpha = 1.0/137.0;
 const double e2 = 4.0 * M_PI * alpha;
+const double mu_mass = 0.1056; //Muon mass in GeV 
 
-double matrix_element(double costheta){
-	return e2*e2*0.5*(1.0+costheta*costheta);
+// Matrix element and cross-section changed to accomodate com energy closer to muon mass
+double matrix_element(double costheta, double s_sqrt){ //Matrix element changed for energy closer to muon mass
+	double s = s_sqrt*s_sqrt;
+	double beta = 4.0 * pow(mu_mass,2)/s;
+	return e2 * e2 * 0.5*(((1.0 + beta) + (1.0 - beta)*costheta*costheta));
 }
  
-double analytic_cross_section(double s){
-	return (4.0*M_PI*alpha*alpha)/(3.0*s);
+double analytic_cross_section(double s_sqrt){
+	double s = s_sqrt*s_sqrt;
+	double beta = 4.0 * pow(mu_mass,2)/s;
+	return ((4.0*M_PI*alpha*alpha)/(3.0*s)) * sqrt((1.0 - beta))*((1.0 + 0.5*beta));
 }
