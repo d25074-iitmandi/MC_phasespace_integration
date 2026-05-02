61ebadc3c3fea1ae47ff480f4626646aa1ca3acb
Abhas Rathi <d25074@students.iitmandi.ac.in>
2026-05-03 00:31:57 +0530

    Convergence and asymmetry

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,8 +5,14 @@ void save_results(int threads, double time, double speedup) {
     std::ofstream file("results/scaling.txt", std::ios::app);
     file << threads << " " << time << " " << speedup << "\n";
 }
-void save_convergence_results(int logN, double mc_sigma, double a_sigma, double abs_err, double mc_error){
-    std::ofstream file("results/convergence.txt",std::ios::app);
-    file << logN << " "<< mc_sigma << " " << a_sigma << " "<< abs_err << " " << mc_error << "\n";
+void save_convergence_results(int logN, int N, double mc_sigma, double a_sigma, double mc_error, double abs_err, double rel_diff){
+    std::ofstream conv_file("results/convergence.csv",std::ios::app);
+    //conv_file << "logN,N,sigma_mc,sigma_analytic,mc_stat_error,abs_diff,rel_diff_pct\n";
+    conv_file << logN << ","<< N <<","<< mc_sigma << "," << a_sigma << ","<< mc_error << ","<< abs_err << "," << abs_err/a_sigma*100 << "\n";
 
+}
+void save_asymmetry(double sqrt_s, double beta, double sigma_f, double sigma_b, double sigma_total, double a_fb, double a_fb_err, double sig){
+    std::ofstream afb_file("results/afb.csv", std::ios::app);
+    //afb_file << "sqrt_s,beta,sigma_F,sigma_B,sigma_total, A_FB,A_FB_error,significance\n";
+    afb_file << sqrt_s << "," << beta << ","<< sigma_f << "," << sigma_b << "," << sigma_total << "," << a_fb << "," << a_fb_err << "," << sig << "\n";
 }
