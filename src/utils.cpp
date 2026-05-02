d11a9f18b237dfbd0dc98c5b70c0448e5ec56e94
Abhas Rathi <d25074@students.iitmandi.ac.in>
2026-05-02 18:47:54 +0530

    writing convergence data points

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,3 +5,8 @@ void save_results(int threads, double time, double speedup) {
     std::ofstream file("results/scaling.txt", std::ios::app);
     file << threads << " " << time << " " << speedup << "\n";
 }
+void save_convergence_results(int logN, double mc_sigma, double a_sigma, double abs_err, double mc_error){
+    std::ofstream file("results/convergence.txt",std::ios::app);
+    file << logN << " "<< mc_sigma << " " << a_sigma << " "<< abs_err << " " << mc_error << "\n";
+
+}
