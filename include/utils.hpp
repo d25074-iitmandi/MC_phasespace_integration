9a7134c2f56667001aa3f9a34982a335efe576bd
Abhas Rathi <d25074@students.iitmandi.ac.in>
2026-05-02 18:46:33 +0530

    output file for convergence test

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,3 +2,4 @@
 #include <vector>
 
 void save_results(int threads, double time, double speedup);
+void save_convergence_results(int logN, double mc_sigma, double a_sigma, double abs_err, double mc_error);
